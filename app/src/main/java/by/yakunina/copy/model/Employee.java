0cd9c719b672c4537798c25512f492e0c92107d5
alexstasko <alexstaskoby@gmail.com>
2018-06-03 23:21:42 +0300

    Added some services

--- app/src/main/java/by/yakunina/copy/model/Employee.java
+++ app/src/main/java/by/yakunina/copy/model/Employee.java
@@ -1,4 +1,58 @@
 package by.yakunina.copy.model;
 
-public class Employee {
+import by.yakunina.copy.model.support.Entity;
+import org.apache.commons.lang3.builder.Builder;
+
+public class Employee extends Entity {
+
+    private final String name;
+    private final String lastName;
+    private final String title;
+
+    private Employee() {
+        super(-1);
+        this.name = null;
+        this.lastName = null;
+        this.title = null;
+    }
+
+    private Employee(EmployeeBuilder builder) {
+        super(builder.id);
+        this.name = builder.name;
+        this.lastName = builder.lastName;
+        this.title = builder.title;
+    }
+
+    public static class EmployeeBuilder implements Builder<Employee> {
+
+        private int id;
+        private String name;
+        private String lastName;
+        private String title;
+
+        public EmployeeBuilder withId(int pId) {
+            this.id = pId;
+            return this;
+        }
+
+        public EmployeeBuilder withName(String pName) {
+            this.name = pName;
+            return this;
+        }
+
+        public EmployeeBuilder withLastName(String pLastName) {
+            this.lastName = pLastName;
+            return this;
+        }
+
+        public EmployeeBuilder withTitle(String pTitle) {
+            this.title = pTitle;
+            return this;
+        }
+
+        @Override
+        public Employee build() {
+            return new Employee(this);
+        }
+    }
 }
---
+++ app/src/main/java/by/yakunina/copy/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package by.yakunina.copy.service;
+
+import by.yakunina.copy.model.Employee;
+
+public class EmployeeService {
+
+    public int createEmployee(Employee employee) {
+        return -1;
+    }
+
+    public Employee findEmployee(int id) {
+        return new Employee.EmployeeBuilder()
+                .build();
+    }
+
+    public void updateEmployee(Employee employee) {
+
+    }
+
+    public void deleteEmployee(int id) {
+        
+    }
+}
